ea29f589661047a7b7e5a5d5760c0660c490ea1d
Nathaly Ferreira <104204488+FSPNathaly@users.noreply.github.com>
2025-05-09 21:07:51 -0300

    Pequenas alterações de estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Route, Routes } from 'react-router'
 import { Home } from './pages/home'
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,13 +5,13 @@ export const Home = () => {
     <main className="flex items-center justify-center h-screen bg-azul">
       <form action="" className="bg-white rounded-4xl flex flex-col items-center">
         <img src={compesaLogo} className="w-[500px]" />
-        <input type="text" placeholder="Login" className="rounded-[9px] border-[2px] border-azul w-2xs text-azul" />
-        <input type="text" placeholder="Senha" className="rounded-[9px] border-[2px] border-azul w-2xs  text-azul"/>
+        <input type="text" placeholder="Login" className="rounded-[9px] border-[2px] border-azul w-2xs p-[5px] text-azul" />
+        <input type="text" placeholder="Senha" className="rounded-[9px] border-[2px] border-azul w-2xs p-[5px] m-[15px] text-azul"/>
         <div>
           <input type="checkbox" id="conectado" />{" "}
-          <label htmlFor="conectado">Manter-me conectado</label>
+          <label htmlFor="conectado" className="text-azul">Manter-me conectado</label>
         </div>
-        <button>Entrar</button>
+        <button><a href="/">Entrar</a></button>
       </form>
     </main>
   );
